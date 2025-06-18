bfda886e89f93fc6a290a6ec33688c9ba5261fe0
Shay Bushary <shaybush93@gmail.com>
2025-06-18 16:22:01 +0300

    deploy changes

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from routes.twilio_routes import twilio_bp
 from routes.mongo_routes import mongo_bp
 from routes.base_routes import base_bp
+import os
 
 load_dotenv()
 app = Flask(__name__)
@@ -21,4 +22,5 @@ app.register_blueprint(twilio_bp)
 app.register_blueprint(mongo_bp)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
